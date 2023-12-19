94995f52a39945974267e5a1e94d7adf7bdd54b3
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-12-19 14:00:53 +0530

    add: cross origin allow policy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const {ServerConfig, ConnectDB } = require('./config');
 const apiRoutes = require('./routes')
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}))
 
 app.use('/api', apiRoutes);
 app.use('', (req, res)=>{
